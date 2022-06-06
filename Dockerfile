cad47408d789dad908b1c0252f997e6e999c63a3
Aires Almeida <83076437+airesalmeida@users.noreply.github.com>
2022-06-07 00:45:25 +0100

    More changes to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,8 @@ FROM node:lts
 
 WORKDIR /app
 
-EXPOSE 3000
 COPY ./website /app
 RUN npm install
 RUN npm run build 
 
-CMD [ "npm", "run", "serve" ]
-
+CMD npm run serve --port $PORT 
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Aires Almeida',
   tagline: 'CV Aires Almeida',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://aires-cv.herokuapp.com/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
